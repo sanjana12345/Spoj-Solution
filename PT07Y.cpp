ead0f8c8b714f66ea2acf72fef7eee8de15e5df6
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 14:58:59 +0530

    PT07Y - Is it a tree

---
+++ PT07Y.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int BFS(vector<int> graph[],int start,int n)
+{
+  int visited[n+1];
+   queue<int> q;
+   
+   q.push(start);
+   int counter = 0;
+   int node;
+   
+   memset(visited,0,sizeof(visited));
+   
+   while(!q.empty())
+   {
+      node =  q.front();
+      q.pop();
+      visited[node] = 1;
+      
+      for(int i=0; i<graph[node].size() ;++i)
+      {
+         if(visited[graph[node][i]])
+            return 0;
+         else
+         {
+            q.push(graph[node][i]);
+         }   
+      }
+      ++counter;
+   }
+   if(counter != n)
+      return 0;
+   else
+      return 1;
+}
+int main() {
+   ios::sync_with_stdio(false);
+   
+   int n,m,u,v;
+   cin>>n>>m;
+   vector<int> graph[n+1];
+   for(int i=0; i<m; ++i)
+   {
+      cin>>u>>v;
+      graph[u].push_back(v);
+   }
+   
+   if(BFS(graph,1,n))
+      cout<<"YES\n";
+   else
+      cout<<"NO\n";
+   
+   return 0;
+}
