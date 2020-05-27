31d91ebe752f6d5654a8bf4fb49ffe9a652870dc
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 14:48:22 +0530

    PHT - Pigeonhole Tower

---
+++ PHT.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+
+	int i = 1, t;
+	long long n;
+	cin >> t;
+
+	while (t--) {
+		cin >> n;
+		printf("Case %i: %i\n", i++, (int)(sqrt(n + 1) - 1));
+	}
+
+	return 0;
+}
