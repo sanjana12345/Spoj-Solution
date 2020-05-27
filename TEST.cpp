092cfa3264a6f1580860a7465304b1975fc3c0b4
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 14:44:44 +0530

    TEST - Life, the Universe, and Everything

---
+++ TEST.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// your code here
+	while(1)
+	{
+		int n;
+		cin>>n;
+		if(n!=42)
+		{
+			cout<<n<<"\n";
+			
+		}
+		else
+		break;
+	}
+
+	return 0;
+}
