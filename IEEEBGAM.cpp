0c82e181f4e3264e50ed71ff232f7be967f33106
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 14:47:43 +0530

    IEEEBGAM - The Ball Game

---
+++ IEEEBGAM.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int t;
+    double m;
+scanf("%d",&t);
+    while(t--)
+    {
+double n;
+scanf("%lf",&n);
+m=n/(n+1);
+printf("%.8lf\n",m);
+  }
+    return 0;
+}
