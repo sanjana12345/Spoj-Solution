5098dd9523291c2530c67be2c051a7cfffc09547
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 14:26:35 +0530

    CODCHESS - Naya Shatranj (New Chess)

---
+++ CODCHESS.php
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// your code here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int p;
+		cin>>p;
+		if(p%2==0)
+		{
+			cout<<"1"<<"\n";
+		}
+		else
+		cout<<"0"<<"\n";
+	}
+
+	return 0;
+}
